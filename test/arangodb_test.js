0e273d40df4042eceabd16c95d3f6fbafac66c04
Mike Williamson <mike@korora.ca>
2016-01-31 15:16:34 -0500

    Add test for inserting data.

--- test/arangodb_test.js
+++ test/arangodb_test.js
@@ -40,6 +40,18 @@ describe('Arangodb', () => {
     done();
   })
 
+  it('can insert data', async (done) => {
+    let latLng = {"lat":45.4292652,"lng":-75.6900505}
+    let location = {"lat":45.4292652,"lng":-75.6900505,"type":"location","address":"126 York Street, Ottawa, ON K1N, Canada"};
+    let aql = aqlQuery`
+      UPSERT ${latLng} INSERT ${location} UPDATE {} IN vertices RETURN NEW
+    `
+    let cursor = await db.query(aql);
+    let result = await cursor.all();
+    expect(result[0].lat).toEqual(45.4292652);
+    done();
+  });
+
   it('can gather a summary of the technologies', async (done) => {
     let aql = aqlQuery`
     let locations = (WITHIN_RECTANGLE(vertices, 45.49407440402927, -75.48362731933594, 45.34949122231596, -75.94058990478516)) LET summary = ( FOR loc in locations FOR tech in TRAVERSAL(vertices, edges, loc._id, "any", { maxDepth: 2, filterVertices: [ { type: "technology" } ], vertexFilterMethod: [ "exclude" ], uniqueness: {"vertices": "global"}}) COLLECT technology = tech.vertex.name INTO g RETURN {"name": technology, "count": LENGTH(g)}) LET sorted = (FOR s in summary SORT s.count DESC LIMIT 10 RETURN s) LET offices = (FOR loc in locations FOR office in TRAVERSAL(vertices, edges, loc._id, "inbound", { maxDepth: 2, filterVertices: [ { type: "office" } ], vertexFilterMethod: [ "exclude" ], uniqueness: {"vertices": "global"}}) RETURN office ) RETURN {sample_size: LENGTH(offices), "summary": sorted}
