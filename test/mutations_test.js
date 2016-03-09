672730f8dd32a1bd25bba573245c9600191b240c
Mike Williamson <mike@korora.ca>
2016-03-09 16:01:36 -0500

    Refactor tests into separate files

---
+++ test/mutations_test.js
@@ -0,0 +1,120 @@
+require("babel-polyfill");
+
+let request = require('supertest')
+  , dbConfig = require('../arangodb_config')[process.env.NODE_ENV]
+  , db = require('arangojs')(dbConfig)
+  , app = require('../app');
+
+describe('Mutations', () => {
+
+  beforeEach(async () => {
+    let vertex_data = require('./data/vertices').vertices
+    let edge_data = require('./data/edges').edges
+    await db.truncate()
+    let vertices = await db.collection('vertices')
+    await vertices.import(vertex_data)
+    let edges = db.collection('edges')
+    await edges.import(edge_data)
+  })
+
+  afterEach(async () => {
+    await db.truncate()
+  })
+
+  it('creates a new organization without duplicating the location', (done) => {
+    let graphql = `
+      mutation {
+        location:createOrganization(
+          name: "Kivuto Solutions Inc."
+          founding_year: 1997
+          url: "http://kivuto.com/"
+    locations: [{ address: "126 York Street, Ottawa, ON K1N, Canada", lat: 45.4292652, lng: -75.6900505 }],
+          technologies: [{name: "asp.net", category:"language"}, {name: "sql-server", category: "storage"}]
+        ){
+          founding_year
+        }
+      }
+    `
+
+    request(app)
+    .post('/graphql')
+    .set('Content-Type', 'application/json; charset=utf-8')
+    .send(`{"query": ${JSON.stringify(graphql)}}`)
+    .expect({data: {location: {founding_year: 1997}}})
+    .end(done);
+  })
+
+  it('creates an organizations with multiple locations', (done) => {
+    let graphql = `
+      mutation { createOrganization(
+        name: "QlikTech International AB"
+        founding_year: 1993
+        url: "http://www.qlik.com"
+        locations: [
+          {address: "390 March Rd, Kanata, ON K2K 3H4, Canada", lat: 45.34036709999999, lng: -75.9117723},
+          {address: "26 Sóltún, Austurbær Reykjavík, Iceland", lat: 64.144207, lng: -21.893473},
+          {address: "Scheelevägen 24, 223 63 Lund, Sweden", lat: 55.7163564, lng: 13.2212684},
+          {address: "Via Stella, 11, 36070 Stella VI, Italy", lat: 45.5952034, lng: 11.3200602}
+        ]
+        technologies: [
+          {name: "c++", category: "language"},
+          {name: "java", category: "language"},
+          {name: "docker", category: "tools"},
+          {name: "cassandra", category: "storage"},
+          {name: "angular", category: "framework"},
+          {name: "javascript", category: "language"},
+          {name: "nodejs", category: "tools"},
+          {name: "qt", category: "library"},
+          {name: "cordova", category: "tools"}
+        ]
+      ) {
+        name
+        founding_year
+        url
+      }}
+    `
+
+    request(app)
+    .post('/graphql')
+    .set('Content-Type', 'application/json; charset=utf-8')
+    .send(`{"query": ${JSON.stringify(graphql)}}`)
+    .end(() => {
+
+     let locationsAroundReykjavík = `
+        query getLocations($neLat: Float, $neLng: Float, $swLat: Float, $swLng: Float) {
+          locations_within_bounds(ne_lat: $neLat, ne_lng: $neLng, sw_lat: $swLat, sw_lng: $swLng){
+            id
+            lat
+            lng
+            address
+            organizations {
+              name
+              technologies {
+                name
+              }
+            }
+          }
+        }
+      `
+
+      let variables = {
+        "neLat":64.19786068472125,
+        "neLng":-21.709671020507812,
+        "swLat":64.08390599654476,
+        "swLng":-22.164573669433594
+      }
+
+      request(app)
+      .post('/graphql')
+      .set('Content-Type', 'application/json; charset=utf-8')
+      .send(JSON.stringify({query: locationsAroundReykjavík, variables: variables }))
+      .expect((res) => {
+        let organizations = res.body.data.locations_within_bounds[0].organizations
+        if (!(organizations.length === 1)) throw new Error(`Expected one organization to be found. Recieved ${organizations}`)
+      })
+      .end(done);
+
+    })
+  })
+})
+
---
+++ test/organizations_test.js
@@ -0,0 +1,42 @@
+require("babel-polyfill");
+
+let request = require('supertest')
+  , dbConfig = require('../arangodb_config')[process.env.NODE_ENV]
+  , db = require('arangojs')(dbConfig)
+  , app = require('../app');
+
+describe('organization queries', () => {
+
+    beforeEach(async () => {
+      let vertex_data = require('./data/vertices').vertices
+      let edge_data = require('./data/edges').edges
+      await db.truncate()
+      let vertices = await db.collection('vertices')
+      await vertices.import(vertex_data)
+      let edges = db.collection('edges')
+      await edges.import(edge_data)
+    })
+
+    afterEach(async () => {
+      await db.truncate()
+    })
+
+    it('serves an organization by name', async (done) => {
+      request(app)
+      .post('/graphql')
+      .set('Content-Type', 'application/graphql; charset=utf-8')
+      .send(`query { organization(name: "Shopify"){founding_year} }`)
+      .expect(`{\n  "data": {\n    "organization": {\n      "founding_year": 2004\n    }\n  }\n}`)
+      .end(done);
+    })
+
+    it('finds technologies for the named organization', async (done) => {
+      request(app)
+      .post('/graphql')
+      .set('Content-Type', 'application/graphql; charset=utf-8')
+      .send(`query { organization(name: "Shopify"){technologies{name}} }`)
+      .expect(`{\n  "data": {\n    "organization": {\n      "technologies": [\n        {\n          "name": "coffeescript"\n        },\n        {\n          "name": "ruby-on-rails"\n        },\n        {\n          "name": "mysql"\n        },\n        {\n          "name": "git"\n        },\n        {\n          "name": "ruby"\n        },\n        {\n          "name": "linux"\n        },\n        {\n          "name": "batman.js"\n        }\n      ]\n    }\n  }\n}`)
+      .end(done);
+    })
+
+});
