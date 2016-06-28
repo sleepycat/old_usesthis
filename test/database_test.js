e8eb7099300bee3f72353ebe6943f144b9aea897
Mike Williamson <mike@korora.ca>
2016-06-28 14:18:14 -0400

    Move beforeEach/afterEach out of inner describe

--- test/database_test.js
+++ test/database_test.js
@@ -15,32 +15,33 @@ let _126_york = "vertices/2733712293"
 
 describe('database functions', () => {
 
-    describe('orgsAndTechnologiesForLocation()', () => {
+  beforeEach(async () => {
+    let vertex_data = require('./data/vertices').vertices
+    let edge_data = require('./data/edges').edges
+    await db.truncate()
+    let vertices = await db.collection('vertices')
+    await vertices.import(vertex_data)
+    let edges = db.collection('edges')
+    await edges.import(edge_data)
+  })
 
-      beforeEach(async () => {
-        let vertex_data = require('./data/vertices').vertices
-        let edge_data = require('./data/edges').edges
-        await db.truncate()
-        let vertices = await db.collection('vertices')
-        await vertices.import(vertex_data)
-        let edges = db.collection('edges')
-        await edges.import(edge_data)
-      })
+  afterEach(async () => {
+    await db.truncate()
+  })
 
-      afterEach(async () => {
-        await db.truncate()
-      })
 
-      it('returns the organizations and languages for the location', async (done) => {
-        //TODO: this test suggests that this function is poorly named.
-        let orgs = await orgsAndTechnologiesForLocation(_126_york, {category: "language"})
-        let technologies = orgs.reduce((prev, curr) => { return prev.concat(curr.technologies)}, [])
-        let categories = technologies.map((tech) => { return tech.category })
-        expect(categories).toNotContain("tools")
-        expect(categories).toNotContain("storage")
-        expect(categories).toNotContain("os")
-        done();
-      })
+  describe('orgsAndTechnologiesForLocation()', () => {
 
+    it('returns the organizations and languages for the location', async (done) => {
+      //TODO: this test suggests that this function is poorly named.
+      let orgs = await orgsAndTechnologiesForLocation(_126_york, {category: "language"})
+      let technologies = orgs.reduce((prev, curr) => { return prev.concat(curr.technologies)}, [])
+      let categories = technologies.map((tech) => { return tech.category })
+      expect(categories).toNotContain("tools")
+      expect(categories).toNotContain("storage")
+      expect(categories).toNotContain("os")
+      done();
     })
+
+  })
 });
