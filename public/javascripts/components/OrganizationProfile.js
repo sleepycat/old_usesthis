d48a904a99bb8fc46d09397eeb94601d9dd2c9bb
Mike Williamson <mike@korora.ca>
2016-06-30 17:47:07 -0400

    Improve styling of organization profile

--- public/javascripts/components/OrganizationProfile.js
+++ public/javascripts/components/OrganizationProfile.js
@@ -3,12 +3,26 @@ import React from 'react'
 export default class OrganizationProfile extends React.Component {
 
   listElements(technologies) {
-    return  technologies.map((tech) => <li key={ btoa(this.props.name + tech.name) } className="technology" >{ tech.name }</li>)
+    return(
+      <p>
+        <h5 className="profile_section_heading" >Technologies used</h5>
+	<ul>
+        {technologies.map((tech) => <li key={ btoa(this.props.name + tech.name) } className="technology" >{ tech.name }</li>)}
+	</ul>
+      </p>
+    )
   }
 
   publicRepos() {
     if(typeof this.props.code == 'string'){
-      return  <p><a key={ btoa(this.props.name + this.props.code) } href={this.props.code} className="repo" >{ this.props.code }</a></p>
+      return(
+        <p>
+          <h5 className="profile_section_heading">Public repos</h5>
+          <a key={ btoa(this.props.name + this.props.code) }
+            href={this.props.code}
+            className="repo_link" >{ this.props.code }</a>
+        </p>
+      )
     }
   }
 
@@ -19,9 +33,7 @@ export default class OrganizationProfile extends React.Component {
 	  <a href={ this.props.url } target="_blank">{ this.props.name }</a>
 	</h3>
         { this.publicRepos() }
-	<ul>
-           { this.listElements(this.props.technologies) }
-	</ul>
+        { this.listElements(this.props.technologies) }
       </div>
     )
   }
