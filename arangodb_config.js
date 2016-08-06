7c1533a6f8e68c791d1292163b069036a234c913
Mike Williamson <mike@korora.ca>
2016-08-06 08:37:13 -0400

    Update to Arangojs 5.0.3

--- arangodb_config.js
+++ arangodb_config.js
@@ -2,13 +2,16 @@ module.exports = {
   "production" : {
     "databaseName": process.env.USESTHIS_PRODUCTION_DB_NAME,
     "url": process.env.USESTHIS_PRODUCTION_DB_URL,
+    "arangoVersion": 28090
   },
   "development" : {
     "databaseName": process.env.USESTHIS_DEVELOPMENT_DB_NAME,
-    "url": process.env.USESTHIS_DEVELOPMENT_DB_URL
+    "url": process.env.USESTHIS_DEVELOPMENT_DB_URL,
+    "arangoVersion": 28090
   },
   "test" : {
     "databaseName": "usesthis_test",
     "url": "http://127.0.0.1:8529",
+    "arangoVersion": 28090
   },
 }
