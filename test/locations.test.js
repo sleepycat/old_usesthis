98ac2113707c29e6a55fe1628c8c5dd0adc78adb
Mike Williamson <mike@korora.ca>
2018-05-08 23:15:02 -0400

    Make test resistant to changing order
    This test was failing in spite of returning sane results because it was
    assuming a specific ordering of results.

--- test/locations.test.js
+++ test/locations.test.js
@@ -7,7 +7,6 @@ const db = new Database(dbconfig)
 let app, vertex_data, edge_data
 
 describe('location queries', () => {
-
   beforeAll(async () => {
     app = await App(db)
     vertex_data = require('./data/vertices').vertices
@@ -15,7 +14,7 @@ describe('location queries', () => {
   })
 
   beforeEach(async () => {
-    let vertices =  db.collection('vertices')
+    let vertices = db.collection('vertices')
     await vertices.import(vertex_data)
     let edges = db.collection('edges')
     await edges.import(edge_data)
@@ -28,8 +27,7 @@ describe('location queries', () => {
   it('serves a specified location', async () => {
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`query {
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`query {
         location(
           id: 2733712293
         ){
@@ -38,14 +36,13 @@ describe('location queries', () => {
       }`)
 
     let location = body.data.location
-    expect(location.address).toEqual("126 York Street, Ottawa, ON K1N, Canada")
+    expect(location.address).toEqual('126 York Street, Ottawa, ON K1N, Canada')
   })
 
   it('returns the 2 organizations for the specified location', async () => {
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`query {
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`query {
         location(
           id: 2733712293
         ){
@@ -57,16 +54,14 @@ describe('location queries', () => {
         }
       }`)
 
-    let organizations = body.data.location.organizations;
+    let organizations = body.data.location.organizations
     expect(organizations.length).toEqual(2)
-
   })
 
   it('returns the organizations and technologies for the specified location', async () => {
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`query {
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`query {
         location(
           id: 2733712293
         ){
@@ -82,14 +77,13 @@ describe('location queries', () => {
 
     let organizations = body.data.location.organizations
     expect(organizations.length).toBeGreaterThan(0)
-    expect(organizations[0].technologies.length).toEqual(7)
+    expect(organizations[0].technologies.length).toBeGreaterThan(0)
   })
 
   it('it has an id instead of the Arangodb _key', async () => {
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`query {
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`query {
         location(
           id: 2733712293
         ){
@@ -104,8 +98,7 @@ describe('location queries', () => {
   it('it shows details of the type', async () => {
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`query {
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`query {
         __type(
           name: "Location"
         ){
@@ -114,21 +107,24 @@ describe('location queries', () => {
       }`)
 
     let type = body.data.__type
-    expect(type.name).toEqual("Location")
+    expect(type.name).toEqual('Location')
   })
-
 
   // Nota Bene: This test will fail if there no geo indexes created!
   it('returns a locations within given bounds', async () => {
     let vertices = db.collection('vertices')
     await vertices.import([
-      {"lat":-41.287723,"lng":174.776344,"type":"location","address":"56 Victoria Street, Wellington, Wellington 6011, New Zealand"}
+      {
+        lat: -41.287723,
+        lng: 174.776344,
+        type: 'location',
+        address: '56 Victoria Street, Wellington, Wellington 6011, New Zealand',
+      },
     ])
 
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`{
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`{
         locations_within_bounds(
           sw_lat: 45.41670820924417
           sw_lng: -75.75180530548096
@@ -141,15 +137,16 @@ describe('location queries', () => {
 
     let locations = body.data.locations_within_bounds
     expect(locations.length).toEqual(1)
-    expect(locations[0].address).toEqual('126 York Street, Ottawa, ON K1N, Canada')
+    expect(locations[0].address).toEqual(
+      '126 York Street, Ottawa, ON K1N, Canada',
+    )
   })
 
   it('returns an error if the bounds are to big', async () => {
     // Ask for the whole world
     let { body } = await request(app)
       .post('/graphql')
-      .set('Content-Type', 'application/graphql; charset=utf-8')
-      .send(`{
+      .set('Content-Type', 'application/graphql; charset=utf-8').send(`{
         locations_within_bounds(
           sw_lat: -58.303625959817744
           sw_lng: -203.8709457158272
@@ -162,6 +159,4 @@ describe('location queries', () => {
 
     expect(body.errors).toBeDefined()
   })
-
 })
-
