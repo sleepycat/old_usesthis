c42308e9fd435151fe36f887ae1122b59e44866f
Mike Williamson <mike@korora.ca>
2018-07-31 10:36:46 -0400

    Refactor database config
    Move u/p into arangodb_config.js with everything else.

--- arangodb_config.js
+++ arangodb_config.js
@@ -1,20 +1,26 @@
 module.exports = {
-  "production" : {
-    "databaseName": process.env.USESTHIS_PRODUCTION_DB_NAME,
-    "url": process.env.USESTHIS_PRODUCTION_DB_URL,
-    "graph": "usesthis",
-    "arangoVersion": 30000
+  production: {
+    databaseName: process.env.USESTHIS_PRODUCTION_DB_NAME,
+    url: process.env.USESTHIS_PRODUCTION_DB_URL,
+    user: process.env.USESTHIS_DB_USER,
+    password: process.env.USESTHIS_DB_PASSWORD,
+    graph: 'usesthis',
+    arangoVersion: 30000,
   },
-  "development" : {
-    "databaseName": process.env.USESTHIS_DEVELOPMENT_DB_NAME,
-    "url": process.env.USESTHIS_DEVELOPMENT_DB_URL,
-    "graph": "usesthis",
-    "arangoVersion": 30000
+  development: {
+    databaseName: process.env.USESTHIS_DEVELOPMENT_DB_NAME,
+    url: process.env.USESTHIS_DEVELOPMENT_DB_URL,
+    user: process.env.USESTHIS_DB_USER,
+    password: process.env.USESTHIS_DB_PASSWORD,
+    graph: 'usesthis',
+    arangoVersion: 30000,
   },
-  "test" : {
-    "databaseName": "usesthis_test",
-    "url": process.env.USESTHIS_TEST_DB_URL,
-    "graph": "test",
-    "arangoVersion": 30000
+  test: {
+    databaseName: 'usesthis_test',
+    url: process.env.USESTHIS_TEST_DB_URL,
+    user: process.env.USESTHIS_DB_USER,
+    password: process.env.USESTHIS_DB_PASSWORD,
+    graph: 'test',
+    arangoVersion: 30000,
   },
 }
